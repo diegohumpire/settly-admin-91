90f05201bc4b60df565fb2b8e5f02cc2aad43506
Diego Humpire <humpire.diego@gmail.com>
2021-10-23 01:40:55 -0500

    Create new account

--- public/js/app.js
+++ public/js/app.js
@@ -2241,100 +2241,168 @@ function App() {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   "default": () => (/* binding */ CreateAccount)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var _BaseForm__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../BaseForm */ "./resources/js/src/BaseForm.jsx");
-/* harmony import */ var _Wrapper__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../Wrapper */ "./resources/js/src/Wrapper.jsx");
-/* harmony import */ var _CreateAccount_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./CreateAccount.scss */ "./resources/js/src/Auth/CreateAccount.scss");
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var _BaseForm__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../BaseForm */ "./resources/js/src/BaseForm.jsx");
+/* harmony import */ var _Wrapper__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../Wrapper */ "./resources/js/src/Wrapper.jsx");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/esm/react-router.js");
+/* harmony import */ var _CreateAccount_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./CreateAccount.scss */ "./resources/js/src/Auth/CreateAccount.scss");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
 
 
-var CreateAccount = /*#__PURE__*/function (_Component) {
-  _inherits(CreateAccount, _Component);
 
-  var _super = _createSuper(CreateAccount);
 
-  function CreateAccount() {
-    _classCallCheck(this, CreateAccount);
 
-    return _super.apply(this, arguments);
-  }
+function CreateAccount() {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)({
+    name: '',
+    surname: '',
+    email: '',
+    email_confirmation: '',
+    password: '',
+    password_confirmation: ''
+  }),
+      _useState2 = _slicedToArray(_useState, 2),
+      newAccount = _useState2[0],
+      setNewAccount = _useState2[1];
 
-  _createClass(CreateAccount, [{
-    key: "render",
-    value: function render() {
-      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_Wrapper__WEBPACK_IMPORTED_MODULE_2__["default"], {
-        title: "Create your account",
-        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("div", {
-          className: "CreateAccount",
-          children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)(_BaseForm__WEBPACK_IMPORTED_MODULE_1__.BaseForm, {
-            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
-              type: "text",
-              placeholder: "First name"
-            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
-              type: "text",
-              placeholder: "Surname"
-            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
-              type: "email",
-              autoComplete: "off",
-              autoCorrect: "off",
-              placeholder: "Email Address"
-            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
-              type: "email",
-              autoComplete: "off",
-              autoCorrect: "off",
-              placeholder: "Confirm Email Address"
-            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
-              type: "password",
-              placeholder: "Password"
-            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("input", {
-              type: "password",
-              placeholder: "Repeat your password"
-            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("div", {
-              className: "Buttons",
-              children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)("button", {
-                className: "Button",
-                children: "Create Account"
-              })
-            })]
-          })
-        })
-      });
+  var history = (0,react_router_dom__WEBPACK_IMPORTED_MODULE_6__.useHistory)();
+
+  var createAccount = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee() {
+      var request, data;
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.next = 2;
+              return axios.post("/api/user", newAccount, {
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'appplication/json'
+                }
+              })["catch"](function (error) {
+                return error;
+              });
+
+            case 2:
+              request = _context.sent;
+              data = request.data.data;
+              console.log(data);
+
+            case 5:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function createAccount() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  var submitHandler = function submitHandler(e) {
+    e.preventDefault();
+
+    try {
+      createAccount();
+      history.push('/');
+    } catch (err) {
+      console.error(err);
     }
-  }]);
+  };
 
-  return CreateAccount;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
+  var onChangeHandler = function onChangeHandler(e) {
+    var _e$target = e.target,
+        name = _e$target.name,
+        value = _e$target.value;
+    setNewAccount(function (newAccount) {
+      return _objectSpread(_objectSpread({}, newAccount), {}, _defineProperty({}, name, value));
+    });
+  };
 
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CreateAccount);
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(_Wrapper__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    title: "Create your account",
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("div", {
+      className: "CreateAccount",
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)(_BaseForm__WEBPACK_IMPORTED_MODULE_2__.BaseForm, {
+        onSubmit: submitHandler,
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("input", {
+          type: "text",
+          name: "name",
+          onChange: onChangeHandler,
+          placeholder: "First name"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("input", {
+          type: "text",
+          name: "surname",
+          onChange: onChangeHandler,
+          placeholder: "Surname"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("input", {
+          type: "email",
+          name: "email",
+          onChange: onChangeHandler,
+          autoComplete: "off",
+          autoCorrect: "off",
+          placeholder: "Email Address"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("input", {
+          type: "email",
+          name: "email_confirmation",
+          onChange: onChangeHandler,
+          autoComplete: "off",
+          autoCorrect: "off",
+          placeholder: "Confirm Email Address"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("input", {
+          type: "password",
+          name: "password",
+          onChange: onChangeHandler,
+          placeholder: "Password"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("input", {
+          type: "password",
+          name: "password_confirmation",
+          onChange: onChangeHandler,
+          placeholder: "Repeat your password"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("div", {
+          className: "Buttons",
+          children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("button", {
+            className: "Button",
+            children: "Create Account"
+          })
+        })]
+      })
+    })
+  });
+}
 
 /***/ }),
 
--- resources/js/src/Auth/CreateAccount.jsx
+++ resources/js/src/Auth/CreateAccount.jsx
@@ -1,29 +1,67 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { BaseForm } from "../BaseForm";
 import Wrapper from "../Wrapper";
+import { useHistory } from "react-router-dom";
 
 import "./CreateAccount.scss";
 
-class CreateAccount extends Component {
-    render() {
-        return (
-            <Wrapper title="Create your account">
-                <div className="CreateAccount">
-                    <BaseForm>
-                        <input type="text" placeholder="First name"/>
-                        <input type="text" placeholder="Surname"/>
-                        <input type="email" autoComplete="off" autoCorrect="off" placeholder="Email Address"/>
-                        <input type="email" autoComplete="off" autoCorrect="off" placeholder="Confirm Email Address"/>
-                        <input type="password" placeholder="Password"/>
-                        <input type="password" placeholder="Repeat your password"/>
-                        <div className="Buttons">
-                            <button className="Button">Create Account</button>
-                        </div>
-                    </BaseForm>
-                </div>
-            </Wrapper>
-        )
-    }
-}
+export default function CreateAccount() {
+    const [newAccount, setNewAccount] = useState({
+        name: '',
+        surname: '',
+        email: '',
+        email_confirmation: '',
+        password: '',
+        password_confirmation: ''
+    });
+    const history = useHistory();
 
-export default CreateAccount;
+    const createAccount = async () => {
+        const request = await axios.post(`/api/user`, newAccount, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'appplication/json'
+            }
+        }).catch(error => error);
+        const data = request.data.data;
+
+        console.log(data);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        try {
+            createAccount();
+            history.push('/');
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setNewAccount((newAccount) => ({
+            ...newAccount, 
+            [name]: value
+        }))
+    }
+
+    return (
+        <Wrapper title="Create your account">
+            <div className="CreateAccount">
+                <BaseForm onSubmit={ submitHandler }>
+                    <input type="text" name="name" onChange={onChangeHandler} placeholder="First name"/>
+                    <input type="text" name="surname" onChange={onChangeHandler} placeholder="Surname"/>
+                    <input type="email" name="email" onChange={onChangeHandler} autoComplete="off" autoCorrect="off" placeholder="Email Address"/>
+                    <input type="email" name="email_confirmation" onChange={onChangeHandler} autoComplete="off" autoCorrect="off" placeholder="Confirm Email Address"/>
+                    <input type="password" name="password" onChange={onChangeHandler} placeholder="Password"/>
+                    <input type="password" name="password_confirmation" onChange={onChangeHandler} placeholder="Repeat your password"/>
+                    <div className="Buttons">
+                        <button className="Button">Create Account</button>
+                    </div>
+                </BaseForm>
+            </div>
+        </Wrapper>
+    )
+}
